ed110a80eccea079a16be65e1b5ce7ff65eaa127
liutao <gg.cn.lt@gmail.com>
2023-08-03 16:19:46 +0800

    support test

--- tests/Feature/Controller/IndexTest.php
+++ tests/Feature/Controller/IndexTest.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Controller;
 
-
-use League\Route\Route;
-use League\Route\Router;
-use MicroPHP\Framework\Application;
 use MicroPHP\Framework\Testing\TestCase;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class IndexTest extends TestCase
 {
-    public function test_index()
+    public function testIndex()
     {
         $result = $this->get('http://127.0.0.1:8080')->assertOk();
         $this->assertSame('Hello World', $result->getContent());
