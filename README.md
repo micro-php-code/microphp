6ab1a428e5a2cc68eb479c86199c82df6fcdd7c3
liutao <gg.cn.lt@gmail.com>
2023-09-07 12:00:33 +0800

    add workerman driver

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-#### 安装roadrunner
+### 安装驱动
+#### 一. 安装workerman驱动
 ```shell
-  php bin/console roadrunner:get
+    composer require microphp/workerman
 ```
+
+#### 二. 安装roadrunner驱动
+```shell
+    composer require microphp/roadrunner
+```
+
+1. 安装roadrunner二进制文件
+    ```shell
+      php bin/console roadrunner:get
+    ```
+2. 修改.env中驱动
+    ```dotenv
+      SERVER_DRIVER=workerman
+    ```
 
 #### 运行
 ```shell
--- config/app.php
+++ config/app.php
@@ -3,15 +3,21 @@
 declare(strict_types=1);
 
 use MicroPHP\Framework\Env;
+use MicroPHP\Framework\Http\Enum\Driver;
 
 return [
     'app_name' => Env::get('APP_NAME', 'MicroPHP'),
     'app_env' => Env::get('APP_ENV', 'local'),
-    'app_url' => Env::get('APP_URL', 'http://127.0.0.1:8080'),
     'scanner' => [
         'directories' => [
             'app',
             'vendor/microphp/framework/src',
         ],
+    ],
+    'server' => [
+        'driver' => Env::get('SERVER_DRIVER', Driver::WORKERMAN),
+        'host' => '0.0.0.0',
+        'port' => 8080,
+        'workers' => 8,
     ],
 ];
